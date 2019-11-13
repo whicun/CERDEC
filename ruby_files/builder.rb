d46bfb3dc8676a5a91f37373769221e03c4fd38c
Erik Benton <erik.benton@hotmail.com>
2019-11-13 15:18:31 -0500

    finishing ruby builder that creates a 'cerdec.txt' of all the linked files

--- ruby_files/builder.rb
+++ ruby_files/builder.rb
@@ -9,19 +9,20 @@
 #   Get/Set
 #   Input/Output
 
-files = ["headers.txt", "constants.txt", "structures.txt", "main.txt",
-        "drawing_functions.txt", "math.txt", "get_set.txt", "input_output.txt"]
-
-path_to_files = "../txt_files/"
 
 def builder
-  final_file = File.open("cerdec.txt", "w")
+  files = ["headers.txt", "constants.txt", "structures.txt",
+          "main.txt", "drawing_functions.txt", "math.txt",
+          "get_set.txt", "input_output.txt"]
+  path_to_files = "../txt_files/"
+  final_file = File.open("../cerdec.txt", "w")
   files.each do |file|
     current_file = File.open(path_to_files + file, "r")
-    file_data = file.readlines
+    file_data = current_file.readlines
     file_data.each do |line|
       final_file.write(line)
     end
+    final_file.write("\n\n")
     current_file.close
   end
   final_file.close
